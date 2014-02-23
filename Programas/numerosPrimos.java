aad00cfc0c47d368a86974ebaf3659cb3630287f
arhoffmannb <arhoffmannb@gmail.com>
2014-02-22 18:23:18 -0600

    No sale, cambio de estrategia

--- Programas/numerosPrimos.java
+++ Programas/numerosPrimos.java
@@ -7,9 +7,9 @@ public class numerosPrimos {
 	int contadorPrueba = 1;
 	int contador = 1;
 
-	for (contador = 1; contador < (numero+1); contador++) { /* Ciclo que muestra todos los números primos desde 1 hasta el número ingresado */
-  	   for (contadorPrueba=1; contadorPrueba<(contador); contadorPrueba++){ /* Ciclo que prueba si el número actual es primo */
-	      if(prueba%contadorPrueba == 0){
+	for (contador = 1; contador < (numero+1); contador++) { // Ciclo que muestra todos los números primos desde 1 hasta el número ingresado 
+  	   for (contadorPrueba=1; contadorPrueba<(contador); contadorPrueba++){ // Ciclo que prueba si el número actual es primo 
+	      if(contador%contadorPrueba == 0){
 	      resultado++;
 	      }
 	   }
