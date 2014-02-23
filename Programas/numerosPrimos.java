c5ef20af3fb80b5c7913f49d91f0e5171c38ed3c
arhoffmannb <arhoffmannb@gmail.com>
2014-02-22 20:36:03 -0600

    Programa numerosPrimos terminado

--- Programas/numerosPrimos.java
+++ Programas/numerosPrimos.java
@@ -1,24 +1,28 @@
 public class numerosPrimos {
     public static void main(String[] args) {
 
+	int resultado = 0; 
+	int contador =1;
+	int numeroActual =0;
 	int numero = Integer.parseInt(args[0].toString());
-	int prueba = 1;
-	int resultado = 0;
-	int contadorPrueba = 1;
-	int contador = 1;
 
-	for (contador = 1; contador < (numero+1); contador++) { // Ciclo que muestra todos los números primos desde 1 hasta el número ingresado 
-  	   for (contadorPrueba=1; contadorPrueba<(contador); contadorPrueba++){ // Ciclo que prueba si el número actual es primo 
-	      if(contador%contadorPrueba == 0){
+	System.out.print("1,");
+	
+	for( numeroActual=1; numeroActual < (numero+1); numeroActual++){
+	resultado = 0;
+	
+	   for( contador=1; contador < (numeroActual+1); contador++){
+	   if(numeroActual%contador == 0){
 	      resultado++;
-	      }
+	      } 
 	   }
 
-	   if(resultado==2){
-	      System.out.println(""+prueba+", ");
-
-	   }
-
+	if(resultado!=2){
+	   System.out.print("");
+	}else{
+	   System.out.print(""+numeroActual+",");
+	}
+	
 	}
     }
 }
